c7f76aae5b31b90113f764f4c3588b9a85dec2e2
Nur Hafizuddin Kamaruddin <hafizuddin95@hotmail.com>
2019-10-07 22:12:25 +0800

    Added Sql file (schema.sql)

---
+++ App/sql/schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE  users (
+	username varchar(256) PRIMARY KEY,
+	password varchar(256) NOT NULL,
+	gender	varchar(1) NOT NULL,
+	phone_num varchar(50) NOT NULL,
+	email varchar(256) NOT NULL,
+	display_name varchar(256) NOT NULL
+);
+
+INSERT INTO users VALUES ('U1', 'password1', 'M', '88888888','user1@gmail.com','User 1'); 
