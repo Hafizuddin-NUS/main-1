e85433d9dd57b6f98ee3905830bc1df0cfa52346
Nur Hafizuddin Kamaruddin <hafizuddin95@hotmail.com>
2019-10-07 22:06:09 +0800

    User account signup V1 (working): Able to create user into database by json

--- App/auth/index.js
+++ App/auth/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
 
 const { Pool } = require('pg')
 const pool = new Pool({
@@ -15,7 +16,7 @@ router.get('/', (req,res) => {
 
 /* SQL Query */
 var sql_query = 'SELECT * FROM users WHERE';
-var sql_query2 = 'SELECT email FROM users WHERE email = ';
+var add_user_query = 'INSERT INTO users VALUES';
 
 router.post('/signup', (req, res) => {
     var check_username_query = sql_query +" username = '" + req.body.username + "' OR email = '"+ req.body.email + "'" +';';
@@ -25,9 +26,25 @@ router.post('/signup', (req, res) => {
                 message : 'ERROR'
             }); 
         }
+        //unique new username and email 
         else if(data.rows.length == 0) {
-            res.json({
-                message : "Username is available."
+            bcrypt.hash(req.body.password, 10).then((hash) => {
+                
+                //add user to database
+                var username = req.body.username;
+                var password = hash;
+                var gender = req.body.gender;
+                var phone_num = req.body.phone_num;
+                var email = req.body.email;
+                var display_name = req.body.display_name;
+
+                // Construct Specific SQL Query
+                var insert_new_user = add_user_query + "('" + username + "','" + hash + "','" + gender + "','" + phone_num + "','" + email + "','" + display_name + "')";
+                pool.query(insert_new_user, (err, data) => {
+                    res.json({
+                        message : 'User account created!'
+                    });
+                }); 
             });
         }
         else if(data.rows.length > 0) {
